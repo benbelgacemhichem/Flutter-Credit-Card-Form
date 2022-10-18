9271f9697961cfbb9741dd338ec1bb0c3863ed53
benbelgacemhichem <benbelgacemhichem@gmail.com>
2022-10-18 18:49:53 +0300

    fix conf

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Dibsy Example',
       debugShowCheckedModeBanner: false,
       theme: appThemeData(context),
-      home: const CreditCardForm(),
+      home: const HomeScreen(),
     );
   }
 }
